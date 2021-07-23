e6a6b9f3cd37def2c7275f16fe16aba75152c77f
Haoyuan Li <hylli@ucdavis.edu>
2021-07-23 15:37:29 -0700

    copy figures into shared folder

--- bash_scripts/lalatex.sh
+++ bash_scripts/lalatex.sh
@@ -6,7 +6,12 @@
 ################################################################################
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
-source "${UsrBinDIR}/bash_scripts/utilities.sh"  # use utility functions
+source "${dir}/utilities.sh"  # use utility functions
+# source the local file list
+host_name=`hostname`
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+[[ -e "${dir}/../files/${host_name}.sh" ]] || { "There is no ${host_name}.sh in the files directory, you need to first creat one"; exit 1; }
+source "${dir}/../files/${host_name}.sh"
 
 usage(){
   # usage of this script
@@ -17,13 +22,15 @@ help to generate latex essays & documentations
 
 Dependencies:
    env:
-       ASPECT_LAB_DIR
-       ASPECT_SOURCE_DIR
 
-Example Usage:
-   document a figure:
-     ./bash_scripts/lalatex.sh document_figure \
-     /home/lochy/ASPECT_PROJECT/TwoDSubduction/non_linear32/eba1_MRf12_iter20_DET/img/CDPT_newton_history.png \
+   files: this script depends on the bash flle named with hostname in the files folder(e.g. shilofue.sh), \n\
+   those files set up variables for the file systems in these PCs\n\
+\n\
+\n\
+Example Usage: \n\
+   document a figure: \n\
+     ./bash_scripts/lalatex.sh document_figure \n\
+     /home/lochy/ASPECT_PROJECT/TwoDSubduction/non_linear32/eba1_MRf12_iter20_DET/img/CDPT_newton_history.png \n\
      /home/lochy/Documents/papers/documented_files/TwoDSubduction
 "
     printf "${_text}"
@@ -49,7 +56,7 @@ Example Usage:
 
 document_figure(){
     ###
-    # document a single file for later usage, todo
+    # document a single file for later usage
     # Inputs:
     # Global variables as input:
     # Return:
@@ -70,12 +77,36 @@ document_figure(){
     # copy a tex file with the same name
     # substitue keys with values
     local keys=("FIGURE_PATH" "FIGURE_BASENAME")
-    local values=("figures/${file_base}" "${file_name}") 
+    local values=("figures/${file_base}" "${file_name}")
     cecho ${GOOD} "Translating figure.tex file"
     utilities_tranlate_script "${UsrBinDIR}/files/tex_files/figure.tex" "${target_dir}/figure.tex"
     # open up gui for editing
     eval "xdg-open ${target_dir}/figure.tex"
     return 0
+}
+
+
+share_figure(){
+    ###
+    # copy a figure to share folder, todo
+    # Inputs:
+    #    $1: name of a figure
+    # Global variables as input:
+    #    SHARED_DIR: a shared directory between pcs
+    # Return:
+    # Global variables as output(changed):
+    ###
+    #SHARED_DIR=''
+    [[ -e "${SHARED_DIR}" ]] || { cecho "${BAD}" "no variable named SHARED_DIR is defined previously"; exit 1; }
+    file_path=`readlink -f "$1"`
+    [[ -e "${file_path}" ]] || { cecho "${FUNCNAME[0]}, no such file ${file_path}"; exit 1; }
+    local dir_name=`dirname "${file_path}"`
+    local dir_base_name=`basename "${dir_name}"`
+    local file_name=$(basename "${file_path}")
+    local file_copy_path="${SHARED_DIR}/${dir_base_name}_${file_name}"  # use dirname + filename as new file name
+    echo "${file_copy_path}"  # debug
+    eval "cp ${file_path} ${file_copy_path}"
+    printf "${FUNCNAME[0]}: new file generated ${file_copy_path}\n" # output
 }
 
 
@@ -92,6 +123,13 @@ main(){
         # Terninal Outputs
         ##
         document_figure "$2" "$3"
+    elif [[ "$1" = "share_figure" ]]; then
+        ##
+        # copy a figure to share folder
+        # Inputs:
+        # Terninal Outputs
+        ##
+        share_figure "$2"
     else
 	cecho "${BAD}" "option ${1} is not valid\n"
     fi
