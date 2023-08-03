2bfbe4f1152867b405e453f15512f09cd05fd2b0
Haoyuan Li <hylli@ucdavis.edu>
2023-08-03 12:27:54 +0800

    add python script file and tests

---
+++ python_script/__main__.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import argparse
+import shilofue
+import shilofue.TwoDSubduction as _TwoDSubduction
+
+def main():
+    """
+    todo
+    """
+    # parse options and arguments
+    pass
+
+
+if __name__ == "__main__":
+    main()
