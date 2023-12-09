3163d91b1d8a45de84ae2e844bfc65599434ba90
Haoyuan Li <hylli@ucdavis.edu>
2023-12-08 19:09:45 -0800

    add etc/alias.sh

---
+++ etc/alias.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+# alias for paraview server
+alias paraview_server="pvserver --server-port=12345"
+# alias for ssh connection with paraview
+alias ssh_ucd_paraview="ssh -L 11111:169.237.93.63:12345 ucd"
